7d98609a492bd5141778ba49062dfc639dee5fa8
Evan Brisbin <evanbrisbin@gmail.com>
2025-09-13 09:25:15 -0500

    Added user input, commands and arguments

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,28 @@
-use std::io::stdin;
+use std::io::{stdin, stdout, Write};
 use std::process::Command;
 
+
 fn main() {
-    let mut input = String::new();
-    stdin().read_line(&mut input).unwrap();
+        loop {
+        // use the `>` character as the prompt
+        // need to explicitly flush this to ensure it prints before read_line
+        print!("> ");
+        stdout().flush();
 
-    let command = input.trim();
+        let mut input = String::new();
+        stdin().read_line(&mut input).unwrap();
+        //split input by whitespace to read command and arguments
+        let mut parts = input.trim().split_whitespace();
+        let command = parts.next().unwrap();
+        let args = parts;
 
-    Command::new(command)
-        .spawn()
-        .unwrap();
+
+        let mut child = Command::new(command)
+            .args(args)
+            .spawn()
+            .unwrap();
+
+        // don't accept another command until this one completes
+        child.wait(); 
+    }
 }
